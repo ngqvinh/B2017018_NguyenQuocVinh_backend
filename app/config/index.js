9ed12d9804dc25989251cb00cfc9eb66bc894252
ngqvinh <vinhb2017018@student.ctu.edu.vn>
2023-02-25 08:16:15 +0700

    Cai dat cac handler truy xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -1,7 +1,10 @@
 const config = {
     app: {
         port: process.env.PORT || 3000,
-    }
-};
+    },
+
+    db: {
+        uri: process.env.MONGODB_URI || "mongodb://0.0.0.0:27017"
+}}
 
 module.exports = config;
